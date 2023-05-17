9eb5fc4f4c8f5648f42138820b194540a90fb045
Shrey1012 <shrey9157@gmail.com>
2023-05-17 20:32:50 +0530

    Pushing Whole Code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import { AuthProvider } from "../src/contexts/AuthContext"
+
+import Chats from "./components/Chats"
+import Login from "./components/Login"
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Avenir' }}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/chats" element={<Chats/>} />
+            <Route path="/" element={<Login/>} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Chats.jsx
@@ -0,0 +1,93 @@
+import React,{useRef, useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
+
+const Chats = () => {
+  const didMountRef = useRef(false);
+
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+  const { user } = useAuth();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+
+    navigate('/');
+  }
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], 'userPhoto.jpg', {type: 'image/jpeg'})
+  }
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true
+    if(!user || user === null) {
+      navigate('/');
+      return;
+    }
+
+    axios.get('https://api.chatengine.io/users/me', {
+      headers: {
+        "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+        "user-name": user.email,
+        "user-secret": user.uid,
+      }
+    })
+    .then(() => {
+      setLoading(false);
+    })
+    .catch(() => {
+      let formdata = new FormData();
+      formdata.append('email', user.email);
+      formdata.append('username', user.email);
+      formdata.append('secret', user.uid);
+
+      getFile(user.photoURL)
+        .then((avatar) => {
+          formdata.append('avatar', avatar, avatar.name)
+
+          axios.post('https://api.chatengine.io/users',
+            formdata,
+            { headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY }}
+          )
+          .then(() => setLoading(false))
+          .catch((error) => console.log(error))
+
+        })
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  }
+  }, [user, navigate]);
+
+  if(!user || loading) return (<div id="loading-container"> <div id="loading-icon"/> </div>);
+
+  return (
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          ChatterBox
+          </div>
+        <div onClick={handleLogout} className='logout-tab'>
+          Logout
+        </div>
+        </div>
+        <ChatEngine
+          height='calc(100vh - 66px)'
+          projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+          userName={user.email}
+          userSecret={user.uid}
+          offset={6}
+          />
+    </div>
+  )
+}
+
+export default Chats;
